f8d1384eef5161e98b1fa4fb6492fe6a70222da8
muhammad zubair <itxzubair45@gmail.com>
2025-02-01 17:44:12 -0800

    oop

---
+++ ObjectOrientedProgramming/src/com/zuabir/oop/oop1.java
@@ -0,0 +1,49 @@
+package com.zuabir.oop;
+
+public class oop1 {
+    public static void main(String[] args) {
+        System.out.println("the hello word and the main data ");
+        Student zubair = new Student(13,"zuabir shehzad",5.5f);
+        Student zain = new Student();
+        System.out.println(zain.name);
+
+//        System.out.println(zubair.name);
+//        System.out.println(zubair.marks);
+//        System.out.println(zubair.rno);
+        //change the name of zuabnir
+
+        Student random = new Student(zubair);
+        System.out.println(random.name);
+        System.out.println(random.marks);
+//
+    }
+}
+class Student {
+    int rno;
+    String name ;
+    float marks;
+    void changeName(String changename) {
+        this.name = changename;
+    }
+
+    Student (Student other){
+        this.name = other.name ;
+        this.rno = other.rno;
+        this.marks= other.marks;
+
+    }
+    //using this we call the other constructer in a class
+
+    Student(){
+        // internally it replaces it with new Student(12,"zain Azhar",10)
+        this (2,"Zain Azhar",10);
+    }
+    Student(int rno, String name ,float marks){
+        this.rno = rno;
+        this.marks = marks;
+        this.name = name;
+
+    }
+
+
+}
