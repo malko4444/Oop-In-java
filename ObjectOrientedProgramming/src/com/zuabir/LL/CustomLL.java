caae6ed38b31ec1f164e4eca9fca1fe10a5890d7
muhammad zubair <itxzubair45@gmail.com>
2025-08-25 16:12:58 +0500

    reverse the list

--- ObjectOrientedProgramming/src/com/zuabir/LL/CustomLL.java
+++ ObjectOrientedProgramming/src/com/zuabir/LL/CustomLL.java
@@ -138,7 +138,76 @@ public void insertFirst(int val){
         return node;
     }
 
+    private void reverseRecursion( Node node ){
+        if(node == tail){
+            head = tail;
+            return;
+        }
 
+        reverseRecursion(node.next);
+        tail.next = node;
+        tail = node ;
+        tail.next = null;
+    }
+
+private Node reverseUsingTraversal(Node head){
+        Node present = head;
+        Node prev = null;
+        Node upcoming = head.next;
+        while (present != null){
+            present.next = prev;
+            prev = present;
+            present = upcoming;
+            if(upcoming.next != null){
+                upcoming = upcoming.next;
+            }
+        }
+        head = prev;
+        return head;
+}
+    public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+    if(left == right){
+        return head;
+    }
+    ListNode current = head;
+    ListNode prev = null;
+        for (int i = 0; current!=null && i < left-1; i++) {
+            prev = current;
+            current = current.next;
+
+        }
+        ListNode last = prev;
+        ListNode newEnd = current;
+        ListNode next = current.next;
+        for (int i = 0; current != null && i < right-left + 1 ; i++) {
+            current.next = prev;
+            prev = current;
+            current = next;
+            if(next != null){
+                next = next.next;
+            }
+
+
+
+
+
+
+        }
+        if(last != null ){
+            last.next = prev;
+        }else{
+            head = prev;
+        }
+        newEnd.next = current;
+        return head;
+    }
     private class Node{
         private int value;
         private Node next;
